1fbc7808cd07a99e7043c8217eb4b8270a378aa5
wissalbenothmen <wissal.lpeople@gmail.com>
2024-11-13 13:39:44 +0100

    third commit: update fetching data

--- fetch_stock_data.py
+++ fetch_stock_data.py
@@ -4,17 +4,20 @@ import json
 import time
 
 API_KEY = "FXMGLC46AQCWXVE2"
-KAFKA_TOPIC = "apple_news"
+KAFKA_TOPIC = "stock-stream"
 KAFKA_SERVER = "localhost:9092"
 
 def fetch_data():
     url = f"https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=AAPL&apikey={API_KEY}"
     response = requests.get(url)
+    
     if response.status_code == 200:
-        return response.json()['data']
+        print(response.json())  # Print the entire response
+        return response.json().get('data', [])  # Safely get 'data' or return an empty list if 'data' is not present
     else:
         print("Error fetching data", response.status_code)
         return []
+
 
 def produce_data():
     producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER, value_serializer=lambda v: json.dumps(v).encode('utf-8'))
@@ -22,8 +25,8 @@ def produce_data():
         news_data = fetch_data()
         for article in news_data:
             producer.send(KAFKA_TOPIC, article)
-            time.sleep(1)  # Adjust based on rate of data generation
-        time.sleep(60)  # Adjust frequency of API calls
+            time.sleep(1)  
+        time.sleep(60)  
 
 if __name__ == "__main__":
     produce_data()
